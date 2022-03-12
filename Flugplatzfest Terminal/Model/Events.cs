4f81f5736e6527c67d03567eeea45dc748941660
JulianKlempa <72907327+JulianKlempa@users.noreply.github.com>
2022-03-12 22:00:49 +0100

    small changes

--- Flugplatzfest Terminal/Model/Events.cs
+++ Flugplatzfest Terminal/Model/Events.cs
@@ -1,20 +1,15 @@
 ﻿using Flugplatzfest_Terminal.Model.Messages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Flugplatzfest_Terminal.Model
 {
     public delegate void NotifyMessageReceived(TextMessage message);
+
     public class Events
     {
         public event NotifyMessageReceived MessageReceived;
 
-        public virtual void OnMessageReceived(TextMessage message) //protected virtual method
+        public virtual void OnMessageReceived(TextMessage message)
         {
-            //if ProcessCompleted is not null then call delegate
             MessageReceived?.Invoke(message);
         }
     }
