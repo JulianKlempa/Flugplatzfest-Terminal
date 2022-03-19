6a8c0b81e1e4ff4e14ab434a74369db2bd29b493
JulianKlempa <72907327+JulianKlempa@users.noreply.github.com>
2022-03-19 13:14:53 +0100

    Started changing the Menu in Settings

--- Flugplatzfest Terminal/MVVM/ViewModels/SettingsViewModel.cs
+++ Flugplatzfest Terminal/MVVM/ViewModels/SettingsViewModel.cs
@@ -1,33 +1,51 @@
 ﻿using Flugplatzfest_Terminal.MVVM.Commands;
+using Flugplatzfest_Terminal.MVVM.Model;
 using Flugplatzfest_Terminal.MVVM.Services;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 
 namespace Flugplatzfest_Terminal.MVVM.ViewModels
 {
     public class SettingsViewModel : ViewModelBase
     {
-        private string menuText;
-
         public ICommand NavigateBackCommand { get; }
         public ICommand SaveMenuCommand { get; }
 
+        public ObservableCollection<MenuItemViewModel> Menu;
+
+        private readonly List<MenuItemViewModel> menu;
+        private readonly App app;
+        private MenuItemViewModel selectedMenuItemViewModel;
+
         public SettingsViewModel(App app, NavigationService terminalViewNavigationService)
         {
+            this.app = app;
+            menu = new List<MenuItemViewModel>();
+            LoadMenu();
+            Menu = new ObservableCollection<MenuItemViewModel>(menu);
             NavigateBackCommand = new NavigateCommand(terminalViewNavigationService);
             SaveMenuCommand = new SaveMenuCommand(app, this);
             //TODO TelegramToken
         }
 
-        public string MenuString
+        private void LoadMenu()
         {
-            get => menuText;
-            set
+            menu.Clear();
+            foreach (MenuItem menuItem in app.GetMenu().GetMenu())
             {
-                menuText = value;
-                OnPropertyChanged(nameof(MenuString));
+                menu.Add(new MenuItemViewModel(menuItem));
             }
         }
 
-        public App App { get; }
+        public MenuItemViewModel SelectedMenuItemViewModel
+        {
+            get { return selectedMenuItemViewModel; }
+            set
+            {
+                selectedMenuItemViewModel = value;
+                OnPropertyChanged(nameof(Menu));
+            }
+        }
     }
 }
